73d78551fe0af5924017e015562f58c5d7fca844
ronaldpannel <69686310+ronaldpannel@users.noreply.github.com>
2021-12-12 16:47:38 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -3,10 +3,11 @@ document.addEventListener('DOMContentLoaded', () => {
 var checkBtn = document.getElementById('checkBtn')
 var getWordBtn = document.getElementById('getWordBtn')
 var resetBtn = document.getElementById('resetBtn')
+var VoiceSelect = document.getElementById('voiceSelect')
 
-var words = ["cat", "dog", "pet", "apple", "orange", "grape", "lemon", "rose", "house", "hose", "cream", "lolly"]
+var words = ["blackboard","cat","dog","pet","apple","orange","grape","lemon","rose","house","hose","cream","lolly","firefly","exam","entry","Bulb","shade",'toy',"song","dance",'tap',"trap","tape","type","biscuit",'cake',"sandwich","carrot","mushroom","strawberry","blackberry","pear","grass","glass","car","pram","boy","girl","brother","sister","mum","dad","aunt","uncle","nanna","ship","boat","canoe",'paddle',"oar","door","floor","core"]
 
-var randomWord = words[Math.floor(Math.random()*words.length)]
+var randomWord = words[Math.floor(Math.random()* words.length)]
 
 resetBtn.addEventListener('click', function(){
     location.reload()
@@ -16,13 +17,14 @@ var speech = new SpeechSynthesisUtterance()
 
 checkBtn.addEventListener('click', function(){
     var input = document.getElementById('input').value
+    var wordCheck = ','+ randomWord.split('')
     if(input == randomWord){
-        speech.text = 'You are correct!'
+        speech.text = 'You are correct'
         speech.rate = 0.85
     } else if(
         speech.text !== randomWord
     ){
-        speech.text = 'You misspelled your word!' + 'it is actually spelt' +randomWord.split("")
+        speech.text = 'You misspelled your word' + 'it is actually spelt' + wordCheck
         speech.rate = 0.85
     }
     window.speechSynthesis.speak(speech)
@@ -32,9 +34,9 @@ getWordBtn.addEventListener('click', function(){
 
     speech.text = 'your word is' + randomWord
     speech.pitch = 1
-    speech.lang = 'en-us'
+    speech.lang = 'en-US'
     speech.volume = 1
-    speech.rate = 1
+    speech.rate = 0.85
     window.speechSynthesis.speak(speech)
 })
 
